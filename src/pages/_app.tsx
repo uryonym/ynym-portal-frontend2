2e484e27ed4b321a7c0e045fb870baa8c9fd344c
yoneyama ryo <uryonym@gmail.com>
2022-08-31 23:19:20 +0900

    背景を白に固定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import { createTheme, CssBaseline, StyledEngineProvider, ThemeProvider } from '@mui/material'
+import { CssBaseline, StyledEngineProvider } from '@mui/material'
 import { FC } from 'react'
-
-const lightTheme = createTheme({
-  palette: {
-    mode: 'light',
-  },
-})
 
 const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <StyledEngineProvider injectFirst>
-        <CssBaseline />
-        <Component {...pageProps} />
-      </StyledEngineProvider>
-    </ThemeProvider>
+    <StyledEngineProvider injectFirst>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </StyledEngineProvider>
   )
 }
 
