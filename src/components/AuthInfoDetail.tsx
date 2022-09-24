7a13538631d3bdc6fdf1ad68aedf2dd9edbd2cd6
yoneyama ryo <uryonym@gmail.com>
2022-09-24 18:25:26 +0900

    認証情報の更新処理と削除処理を実装

---
+++ src/components/AuthInfoDetail.tsx
@@ -0,0 +1,145 @@
+import ArrowBackIcon from '@mui/icons-material/ArrowBack'
+import { AppBar, Button, IconButton, TextField, Toolbar } from '@mui/material'
+import { Stack } from '@mui/system'
+import axios from 'axios'
+import { ChangeEvent, Dispatch, FC, SetStateAction, useEffect, useState } from 'react'
+
+import { fbAuth } from '@/lib/firebaseConfig'
+import { AuthInfo } from '@/models'
+import styles from '@/styles/TaskDetail.module.scss'
+
+type AuthInfoDetailProps = {
+  authInfo: AuthInfo
+  setAuthInfos: Dispatch<SetStateAction<AuthInfo[]>>
+  onClose: () => void
+}
+
+const AuthInfoDetail: FC<AuthInfoDetailProps> = ({ authInfo, setAuthInfos, onClose }) => {
+  const apiUrl = process.env.NODE_ENV === 'production' ? process.env.productionUrl : process.env.developmentUrl
+
+  const [serviceName, setServiceName] = useState<string>('')
+  const [loginId, setLoginId] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+  const [other, setOther] = useState<string>('')
+
+  const handleChangeServiceName = (e: ChangeEvent<HTMLInputElement>) => {
+    setServiceName(e.target.value)
+  }
+
+  const handleChangeLoginId = (e: ChangeEvent<HTMLInputElement>) => {
+    setLoginId(e.target.value)
+  }
+
+  const handleChangePassword = (e: ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value)
+  }
+
+  const handleChangeOther = (e: ChangeEvent<HTMLInputElement>) => {
+    setOther(e.target.value)
+  }
+
+  const handleSave = () => {
+    fbAuth.currentUser?.getIdToken(true).then((idToken) => {
+      const data = {
+        auth_info: {
+          service_name: serviceName,
+          login_id: loginId,
+          password: password || null,
+          other: other || null,
+        },
+      }
+      const config = {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      }
+      axios
+        .patch(`${apiUrl}/auth_infos/${authInfo.id}`, data, config)
+        .then((response) => {
+          setAuthInfos((prevState) => prevState.map((x) => (x.id === authInfo.id ? response.data : x)))
+          onClose()
+        })
+        .catch((error) => console.log(error))
+    })
+  }
+
+  const handleDelete = () => {
+    fbAuth.currentUser?.getIdToken(true).then((idToken) => {
+      const config = {
+        headers: {
+          Authorization: `Bearer ${idToken}`,
+        },
+      }
+      axios
+        .delete(`${apiUrl}/auth_infos/${authInfo.id}`, config)
+        .then(() => {
+          setAuthInfos((prevState) => prevState.filter((x) => x.id !== authInfo.id))
+          onClose()
+        })
+        .catch((error) => console.log(error))
+    })
+  }
+
+  useEffect(() => {
+    setServiceName(authInfo.service_name)
+    setLoginId(authInfo.login_id)
+    setPassword(authInfo.password || '')
+    setOther(authInfo.other || '')
+  }, [authInfo])
+
+  return (
+    <>
+      <div className={styles.container}>
+        <TextField
+          className={styles.formControl}
+          fullWidth
+          placeholder='サービス名を入力'
+          variant='standard'
+          value={serviceName}
+          onChange={handleChangeServiceName}
+        />
+        <TextField
+          className={styles.formControl}
+          fullWidth
+          placeholder='ログインIDを入力'
+          variant='standard'
+          value={loginId}
+          onChange={handleChangeLoginId}
+        />
+        <TextField
+          className={styles.formControl}
+          fullWidth
+          placeholder='パスワードを入力'
+          variant='standard'
+          value={password}
+          onChange={handleChangePassword}
+        />
+        <TextField
+          className={styles.formControl}
+          fullWidth
+          placeholder='備考を入力'
+          variant='standard'
+          value={other}
+          onChange={handleChangeOther}
+        />
+        <Stack direction='row' justifyContent='space-between'>
+          <Button variant='outlined' onClick={handleSave}>
+            保存
+          </Button>
+          <Button variant='outlined' color='error' onClick={handleDelete}>
+            削除
+          </Button>
+        </Stack>
+      </div>
+      <AppBar className={styles.appbar} position='fixed'>
+        <Toolbar>
+          <IconButton color='inherit' onClick={onClose}>
+            <ArrowBackIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </>
+  )
+}
+
+export default AuthInfoDetail
--- src/pages/authInfo.tsx
+++ src/pages/authInfo.tsx
@@ -1,9 +1,10 @@
-import { Drawer, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { Button, Drawer, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 import type { NextPage } from 'next'
 
+import AuthInfoDetail from '@/components/AuthInfoDetail'
 import AuthInfoNew from '@/components/AuthInfoNew'
 import BottomAppBar from '@/components/BottomAppBar'
 import { useAuthContext } from '@/context/AuthContext'
@@ -15,8 +16,15 @@ const AuthInfo: NextPage = () => {
   const apiUrl = process.env.NODE_ENV === 'production' ? process.env.productionUrl : process.env.developmentUrl
 
   const [isNewOpen, setIsNewOpen] = useState<boolean>(false)
+  const [isDetailOpen, setIsDetailOpen] = useState<boolean>(false)
+  const [authInfo, setAuthInfo] = useState<AuthInfo>({ service_name: '', login_id: '' })
   const [authInfos, setAuthInfos] = useState<AuthInfo[]>([])
   const { currentUser } = useAuthContext()
+
+  const handleClickAuthInfo = (authInfo: AuthInfo) => () => {
+    setAuthInfo(authInfo)
+    setIsDetailOpen(true)
+  }
 
   useEffect(() => {
     fbAuth.currentUser?.getIdToken(true).then((idToken) => {
@@ -38,6 +46,11 @@ const AuthInfo: NextPage = () => {
         <TableCell>{authInfo.login_id}</TableCell>
         <TableCell>{authInfo.password}</TableCell>
         <TableCell>{authInfo.other}</TableCell>
+        <TableCell>
+          <Button variant='text' size='small' onClick={handleClickAuthInfo(authInfo)}>
+            編集
+          </Button>
+        </TableCell>
       </TableRow>
     )
   })
@@ -54,6 +67,7 @@ const AuthInfo: NextPage = () => {
                 <TableCell>ログインID</TableCell>
                 <TableCell>パスワード</TableCell>
                 <TableCell>備考</TableCell>
+                <TableCell>アクション</TableCell>
               </TableRow>
             </TableHead>
             <TableBody>{authInfoRows}</TableBody>
@@ -62,6 +76,9 @@ const AuthInfo: NextPage = () => {
       </div>
       <Drawer anchor='bottom' open={isNewOpen} onClose={() => setIsNewOpen(false)}>
         <AuthInfoNew setAuthInfos={setAuthInfos} onClose={() => setIsNewOpen(false)} />
+      </Drawer>
+      <Drawer className={styles.drawer} anchor='right' open={isDetailOpen} onClose={() => setIsDetailOpen(false)}>
+        <AuthInfoDetail authInfo={authInfo} setAuthInfos={setAuthInfos} onClose={() => setIsDetailOpen(false)} />
       </Drawer>
       <BottomAppBar onAddItem={() => setIsNewOpen(true)} />
     </>
